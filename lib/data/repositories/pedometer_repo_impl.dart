fc36f01bfd8ebac0792cbd7c861f588c0853b76e
dw0124 <doowone8650@naver.com>
2026-02-02 22:22:38 +0900

    refactor: pedometerDeltaStream 전달 방식 개선
    <문제>
    DataSource의 broadcast 스트림에 map을 사용하면서 Single-Subscription으로 변환됨.
    
     이로 인해 여러 곳에서 구독 시 가공 로직이 중복 실행되었고, 공유 변수가 먼저 업데이트되어 이후 구독자들에게 데이터(Delta)가 0으로 전달되는 오류가 발생
    
    <해결 방법>
    Repo 안에서 DataSource 스트림을 직접 구독하고, StreamController.broadcast()을 사용해 가공된 데이터만 전달하도록 수정했음

--- lib/data/repositories/pedometer_repo_impl.dart
+++ lib/data/repositories/pedometer_repo_impl.dart
@@ -1,10 +1,11 @@
+import 'dart:async';
+
 import 'package:run_run/data/data_sources/pedometer_data_source.dart';
 import 'package:run_run/data/extensions/mapper_extension.dart';
 import 'package:run_run/domain/entities/pedometer_delta.dart';
 import 'package:run_run/domain/repositories/pedometer_repository.dart';
 
 class PedometerRepoImpl implements PedometerRepo {
-
   PedometerRepoImpl({required PedometerDataSource dataSource}) : _dataSource = dataSource;
 
   final PedometerDataSource _dataSource;
@@ -16,59 +17,85 @@ class PedometerRepoImpl implements PedometerRepo {
   // ----------------------------------
 
   @override
-  Stream<PedometerDelta> get pedometerDeltaStream => _dataSource.pedometerDataDTOStream.map((pedometerDTO) {
-    final pedometer = pedometerDTO.toPedometer();
+  Stream<PedometerDelta> get pedometerDeltaStream => _deltaStreamController.stream;
 
-    // 1. 시작 시간 결정
-    final start = _lastEndDate ?? pedometer.startDate;
+  final StreamController<PedometerDelta> _deltaStreamController = StreamController<PedometerDelta>.broadcast();
+  StreamSubscription? _sourceSubscription;
 
-    // 2. 걸음수 Delta 계산
-    double deltaSteps = pedometer.numberOfSteps - _lastNumberOfSteps;
-    if (deltaSteps < 0) {
-      deltaSteps = pedometer.numberOfSteps;
-    }
+  Future<void> _reset() async {
+    // 기존 구독 해제
+    await _sourceSubscription?.cancel();
+    _sourceSubscription = null;
 
-    // 3. 거리 Delta 계산
-    double deltaDistance = 0;
-    if (pedometer.distance != null) {
-      deltaDistance = pedometer.distance! - _lastDistance;
-      if (deltaDistance < 0) {
-        deltaDistance = pedometer.distance!;
+    // Delta 계산 변수 초기화
+    _lastEndDate = null;
+    _lastNumberOfSteps = 0;
+    _lastDistance = 0;
+  }
+
+  void _init() {
+    _sourceSubscription = _dataSource.pedometerDataDTOStream.listen((pedometerDTO) {
+      final pedometer = pedometerDTO.toPedometer();
+
+      // 1. 시작 시간 결정
+      final start = _lastEndDate ?? pedometer.startDate;
+
+      // 2. 걸음수 Delta 계산
+      double deltaSteps = pedometer.numberOfSteps - _lastNumberOfSteps;
+      if (deltaSteps < 0) {
+        deltaSteps = pedometer.numberOfSteps;
       }
-    }
 
-    // 4. 상태 업데이트
-    _lastNumberOfSteps = pedometer.numberOfSteps;
-    _lastEndDate = pedometer.endDate;
-    _lastDistance = pedometer.distance ?? _lastDistance;
+      // 3. 거리 Delta 계산
+      double deltaDistance = 0;
+      if (pedometer.distance != null) {
+        deltaDistance = pedometer.distance! - _lastDistance;
+        if (deltaDistance < 0) {
+          deltaDistance = pedometer.distance!;
+        }
+      }
 
-    final pedometerDelta = PedometerDelta(
-      startDate: start,
-      endDate: pedometer.endDate,
-      stepDelta: deltaSteps.toInt(),
-      distanceDelta: deltaDistance,
-      currentCadence: pedometer.currentCadence,
-      currentPace: pedometer.currentPace,
-    );
+      // 4. 상태 업데이트
+      _lastNumberOfSteps = pedometer.numberOfSteps;
+      _lastEndDate = pedometer.endDate;
+      _lastDistance = pedometer.distance ?? _lastDistance;
 
-    return pedometerDelta;
-  });
+      final pedometerDelta = PedometerDelta(
+        startDate: start,
+        endDate: pedometer.endDate,
+        stepDelta: deltaSteps.toInt(),
+        distanceDelta: deltaDistance,
+        currentCadence: pedometer.currentCadence,
+        currentPace: pedometer.currentPace,
+      );
+
+      _deltaStreamController.add(pedometerDelta);
+    });
+  }
 
   @override
   Future<void> start() async {
+    await _reset();
+    _init();
     await _dataSource.start();
   }
 
   @override
   Future<void> pause() async {
+    await _reset();
     await _dataSource.cancel();
   }
 
   @override
   Future<void> cancel() async {
+    await _reset();
     await _dataSource.cancel();
   }
 
   @override
-  void dispose() => _dataSource.dispose();
+  void dispose() {
+    _reset();
+    _deltaStreamController.close();
+    _dataSource.dispose();
+  }
 }
