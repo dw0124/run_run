22c3323d4ba2ac3847f573467c05747d0e7e2d91
dw0124 <doowone8650@naver.com>
2026-03-03 02:53:21 +0900

    feat: 속성 추가, 생성자(파싱) 수정, toDistanceQuantitySample 추가

--- ios/Runner/Model/LocationSample.swift
+++ ios/Runner/Model/LocationSample.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import CoreLocation
+import HealthKit
 
 struct LocationSample {
     let latitude: Double
@@ -23,37 +24,52 @@ struct LocationSample {
     let floor: Int?
     let speed: Double
     let speedAccuracy: Double
+    
+    let previousTimestamp: Date?
+    let distanceDelta: Double?
 
     init?(from dict: [String: Any]) {
+        let previousTimestamp = dict["previousTimestamp"] as? Double
+        let distanceDelta = dict["distanceDelta"] as? Double
+        
         guard
-            let latitude = dict["latitude"] as? Double,
-            let longitude = dict["longitude"] as? Double,
-            let timestampMs = dict["timestamp"] as? Int,
-            let accuracy = dict["accuracy"] as? Double,
-            let altitude = dict["altitude"] as? Double,
-            let altitudeAccuracy = dict["altitude_accuracy"] as? Double,
-            let heading = dict["heading"] as? Double,
-            let headingAccuracy = dict["heading_accuracy"] as? Double,
-            let speed = dict["speed"] as? Double,
-            let speedAccuracy = dict["speed_accuracy"] as? Double
-        else {
-            return nil
-        }
-
+            let position = dict["position"] as? [String: Any],
+            let latitude = position["latitude"] as? Double,
+            let longitude = position["longitude"] as? Double,
+            let timestampMs = position["timestamp"] as? Double,
+            let accuracy = position["accuracy"] as? Double,
+            let altitude = position["altitude"] as? Double,
+            let altitudeAccuracy = position["altitude_accuracy"] as? Double,
+            let heading = position["heading"] as? Double,
+            let headingAccuracy = position["heading_accuracy"] as? Double,
+            let speed = position["speed"] as? Double,
+            let speedAccuracy = position["speed_accuracy"] as? Double
+        else { return nil }
+        
+        let floor = position["floor"] as? Int
+        
         self.latitude = latitude
         self.longitude = longitude
         self.timestamp = Date(timeIntervalSince1970: TimeInterval(timestampMs) / 1000.0)
-
+        
         self.accuracy = accuracy
         self.altitude = altitude
         self.altitudeAccuracy = altitudeAccuracy
-
+        
         self.heading = heading
         self.headingAccuracy = headingAccuracy
-
-        self.floor = dict["floor"] as? Int
+        
+        self.floor = floor
         self.speed = speed
         self.speedAccuracy = speedAccuracy
+        
+        if let prevTimestampMs = previousTimestamp {
+            self.previousTimestamp = Date(timeIntervalSince1970: prevTimestampMs / 1000.0)
+        } else {
+            self.previousTimestamp = nil // 첫 번째 좌표일 경우
+        }
+        
+        self.distanceDelta = distanceDelta
     }
 
     /// HealthKit Route용 CLLocation으로 변환
@@ -72,33 +88,26 @@ struct LocationSample {
             timestamp: timestamp
         )
     }
+    
+    func toDistanceQuantitySample() -> HKQuantitySample? {
+        guard let distanceDelta = distanceDelta else {
+            print("Distance 샘플 생성 오류 - distanceDelta")
+            return nil
+        }
+        
+        guard let previousTimestamp = previousTimestamp else {
+            print("Distance 샘플 생성 오류 - previousTimestamp")
+            return nil
+        }
+        
+        let distanceType = HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!
+        let distanceSample = HKQuantitySample(
+            type: distanceType,
+            quantity: HKQuantity(unit: .meter(), doubleValue: distanceDelta),
+            start: previousTimestamp,
+            end: timestamp
+        )
+        
+        return distanceSample
+    }
 }
-
-//
-//Map<String, dynamic> toJson() => {
-//      'longitude': longitude,
-//      'latitude': latitude,
-//      'timestamp': timestamp.millisecondsSinceEpoch,
-//      'accuracy': accuracy,
-//      'altitude': altitude,
-//      'altitude_accuracy': altitudeAccuracy,
-//      'floor': floor,
-//      'heading': heading,
-//      'heading_accuracy': headingAccuracy,
-//      'speed': speed,
-//      'speed_accuracy': speedAccuracy,
-//      'is_mocked': isMocked,
-//    };
-
-//final double latitude;
-//final double longitude;
-//final DateTime timestamp;
-//final double altitude;
-//final double altitudeAccuracy;
-//final double accuracy;
-//final double heading;
-//final double headingAccuracy;
-//final int? floor;
-//final double speed;
-//final double speedAccuracy;
-//final bool isMocked;
