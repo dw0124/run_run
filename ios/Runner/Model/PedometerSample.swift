5ffb6587a7701389fcccbdb0733db331d4ae1c93
dw0124 <doowone8650@naver.com>
2026-03-03 02:52:00 +0900

    feat: 생성자(파싱) 및  toHKSamples 수정

--- ios/Runner/Model/PedometerSample.swift
+++ ios/Runner/Model/PedometerSample.swift
@@ -10,24 +10,35 @@ import HealthKit
 
 struct PedometerSample {
     init?(from dict: [String: Any], formatter: ISO8601DateFormatter = ISO8601DateFormatter()) {
-        guard let startStr = dict["start"] as? String,
-              let endStr = dict["end"] as? String,
-              let steps = dict["steps"] as? Double,
-              let startDate = formatter.date(from: startStr),
-              let endDate = formatter.date(from: endStr) else {
+        guard let startDateStr = dict["startDate"] as? String,
+              let endDateStr = dict["endDate"] as? String,
+              let stepDelta = dict["stepDelta"] as? Int,
+              let distanceDelta = dict["distanceDelta"] as? Double
+        else {
+            print("PedometerSample Parse Error")
             return nil
         }
         
+        let currentPace = dict["currentPace"] as? Double
+        let currentCadence = dict["currentCadence"] as? Double
+        
+        let endDate = formatter.date(from: endDateStr)!
+        let startDate = formatter.date(from: startDateStr)!
+        
         self.start = startDate
         self.end = endDate
-        self.steps = steps
-        self.distance = dict["steps"] as? Double
+        self.steps = stepDelta
+        self.distance = distanceDelta
+        self.currentPace = currentPace
+        self.currentCadence = currentCadence
     }
     
     let start: Date
     let end: Date
-    let steps: Double
+    let steps: Int
     let distance: Double?
+    let currentPace: Double?
+    let currentCadence: Double?
     
     func toHKSamples() -> [HKQuantitySample] {
         var samples: [HKQuantitySample] = []
@@ -36,12 +47,13 @@ struct PedometerSample {
         let stepType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
         let stepSample = HKQuantitySample(
             type: stepType,
-            quantity: HKQuantity(unit: .count(), doubleValue: steps),
+            quantity: HKQuantity(unit: .count(), doubleValue: Double(steps)),
             start: start,
             end: end
         )
         samples.append(stepSample)
-
+        
+        /* ---- distanceWalkingRunning, runningSpeed 샘플은 LocationSample에서 처리하도록 수정 ----
         if let distance = distance, distance > 0 {
             // distance Sample
             let distanceType = HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning)!
@@ -52,7 +64,7 @@ struct PedometerSample {
                 end: end
             )
             samples.append(distanceSample)
-
+            
             // runngingSpeed Sample
             let duration = end.timeIntervalSince(start) // seconds
             if duration > 0 {
@@ -68,7 +80,9 @@ struct PedometerSample {
                 )
                 samples.append(speedSample)
             }
+             
         }
+        ---- distanceWalkingRunning, runningSpeed 샘플은 LocationSample에서 처리하도록 수정 ---- */
 
         return samples
     }
