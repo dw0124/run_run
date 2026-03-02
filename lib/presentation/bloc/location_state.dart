30ce3c7d93099c86a41732451ca34897473c18c0
dw0124 <doowone8650@naver.com>
2026-03-03 02:40:04 +0900

    feat: LocationState totalDistance 추가

--- lib/presentation/bloc/location_state.dart
+++ lib/presentation/bloc/location_state.dart
@@ -11,6 +11,7 @@ enum LocationStatus {
 class LocationState extends Equatable {
 
   const LocationState({
+    this.totalDistance = 0,
     this.status = LocationStatus.initial,
     this.location,
   });
@@ -18,16 +19,20 @@ class LocationState extends Equatable {
   final LocationStatus status;
   final Location? location;
 
+  final double totalDistance;
+
   LocationState copyWith({
     LocationStatus? status,
     Location? location,
+    double? totalDistance,
   }) {
     return LocationState(
       status: status ?? this.status,
       location: location ?? this.location,
+      totalDistance: totalDistance ?? this.totalDistance,
     );
   }
 
   @override
-  List<Object?> get props => [status, location];
+  List<Object?> get props => [status, location, totalDistance];
 }
