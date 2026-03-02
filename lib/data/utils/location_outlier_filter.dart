915d0532f1fba7b9ee507c5f3f8a6bd5d9d335d6
dw0124 <doowone8650@naver.com>
2026-03-03 02:16:24 +0900

    feat: Position의 Outlier 확인용 함수 추가

--- lib/data/utils/location_outlier_filter.dart
+++ lib/data/utils/location_outlier_filter.dart
@@ -1,3 +1,4 @@
+import 'package:geolocator/geolocator.dart';
 import 'package:run_run/domain/entities/location.dart';
 
 class LocationOutlierFilter {
@@ -26,6 +27,26 @@ class LocationOutlierFilter {
 
     // _lastLocation 업데이트
     _lastLocation = location;
+    return false;
+  }
+
+  bool isOutlierPosition(Position position) {
+
+    // CLLocation speedAccuracy 검사
+    if(position.speedAccuracy < 0) {
+      return true;
+    }
+
+    // Position 데이터의 신뢰도 검사
+    if (position.accuracy > 20) {
+      return true;
+    }
+
+    // CLLocation speed 검사 (음수거나 _maxSpeed 초과 시 이상치)
+    if (position.speed < 0 || position.speed > _maxSpeed) {
+      return true;
+    }
+
     return false;
   }
 
