5bd98bca3d58ac1dc7708f4c9f8d1c31c3f49daf
dw0124 <doowone8650@naver.com>
2026-03-03 02:27:12 +0900

    feat: StreamController를 사용한 broadcast 방식으로 변경 및 좌표사이 거리 계산 함수 추가
    - 이전 좌표와 현재 좌표 사이 거리를 구하기 위한 함수 추가 (_processPosition)
    - StreamController를 사용해 broadcast 방식으로 스트림 전달

--- lib/data/repositories/location_tracking_repo_impl.dart
+++ lib/data/repositories/location_tracking_repo_impl.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
+import 'package:geolocator/geolocator.dart';
 import 'package:run_run/data/data_sources/geolocator_data_source.dart';
 import 'package:run_run/data/extensions/mapper_extension.dart';
-import 'package:run_run/data/utils/kalman_filter.dart';
 import 'package:run_run/data/utils/location_outlier_filter.dart';
 import 'package:run_run/domain/repositories/location_repository.dart';
 import 'package:run_run/domain/entities/location.dart';
@@ -13,32 +15,87 @@ class LocationTrackingRepoImpl implements LocationRepository {
 
   final LocationDataSource _dataSource;
 
-  @override
-  Stream<Location> get locationStream {
-    return _dataSource.positionStream
-        .map((position) => PositionMapper(position).toLocation());
+  final StreamController<Location> _streamController = StreamController<Location>.broadcast();
+  StreamSubscription? _sourceSubscription;
+
+  final _filter = LocationOutlierFilter();
+  Position? _previousPosition;
+
+  void _init() {
+    _sourceSubscription = _dataSource.positionStream.listen((position) {
+      final location = _processPosition(position);
+
+      if(location != null) {
+        _streamController.add(location);
+      }
+    });
   }
 
   @override
+  Stream<Location> get locationStream => _streamController.stream;
+
+  @override
   Future<void> start() async {
+    await _reset();
+    _init();
     await _dataSource.start();
   }
 
   @override
-  void pause() {
-    _dataSource.pause();
+  Future<void> pause() async {
+    await _dataSource.pause();
+    _reset();
   }
 
   @override
   Future<void> cancel() async {
+    _reset();
     await _dataSource.cancel();
   }
 
   @override
-  void dispose() => _dataSource.dispose();
+  void dispose() {
+    _dataSource.dispose();
+    _reset();
+    _streamController.close();
+  }
 
   @override
   void save(Location location) {
 
+  }
+
+  /// 좌표의 이상치를 필터링하고 거리를 계산하여 Location 엔티티로 변환
+  Location? _processPosition(Position position) {
+    // 이상치 필터링
+    if (_filter.isOutlierPosition(position)) return null;
+
+    DateTime? previousTimestamp = _previousPosition?.timestamp;
+    double? distanceDelta;
+
+    // 거리 계산
+    if (_previousPosition != null) {
+      distanceDelta = Geolocator.distanceBetween(
+        _previousPosition!.latitude,
+        _previousPosition!.longitude,
+        position.latitude,
+        position.longitude,
+      );
+    }
+
+    // 상태 업데이트
+    _previousPosition = position;
+
+    return PositionMapper(position)
+        .toLocation(
+        distanceDelta: distanceDelta,
+        previousTimestamp: previousTimestamp
+    );
+  }
+
+  Future<void> _reset() async {
+    await _sourceSubscription?.cancel();
+    _sourceSubscription = null;
+    _previousPosition = null;
   }
 }
