ccc32d0184551acfaa6ca40b3401c75452d117c5
dw0124 <doowone8650@naver.com>
2026-03-04 16:43:26 +0900

    feat: currentPace 프로퍼티 추가, copyWith 수정

--- lib/presentation/bloc/location_state.dart
+++ lib/presentation/bloc/location_state.dart
@@ -14,25 +14,31 @@ class LocationState extends Equatable {
     this.totalDistance = 0,
     this.status = LocationStatus.initial,
     this.location,
+    this.currentPace,
   });
 
   final LocationStatus status;
   final Location? location;
 
   final double totalDistance;
+  final double? currentPace;
 
   LocationState copyWith({
     LocationStatus? status,
     Location? location,
     double? totalDistance,
+    Object? currentPace = _undefined,
   }) {
     return LocationState(
       status: status ?? this.status,
       location: location ?? this.location,
       totalDistance: totalDistance ?? this.totalDistance,
+      currentPace: currentPace == _undefined ? this.currentPace : currentPace as double?,
     );
   }
 
   @override
-  List<Object?> get props => [status, location, totalDistance];
+  List<Object?> get props => [status, location, totalDistance, currentPace];
 }
+
+const _undefined = Object();
